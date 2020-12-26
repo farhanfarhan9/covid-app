0dc6862aff5abd8629d374866712c18b1354ef2c
Farhan <faarhan7@gmail.com>
2020-12-26 11:50:58 +0700

    login selesai

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="container py-4">
+    <div class="card p-3">
+        <div class="row ">
+            <div class="col-md-4">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <h3 class="font-weight-normal text-center">Masuk</h3>
+                    <div class="form-group py-2">
+                        <input id="email" type="email" class="loginregister @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" [autofocus] placeholder="Nama pengguna">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <div class="form-group py-2">
+                        <input id="password" type="password" class="loginregister @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Masukkan Password">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    @if (Route::has('password.request'))
+                        <a href="{{ route('password.request') }}">
+                            <p class="text-muted d-flex flex-md-row-reverse">
+                            {{ __('Forgot Your Password?') }}
+                            </p>
+                        </a>
+                    @endif
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">
+                            {{ __('Login') }}
+                        </button>
+                    </div>
+                    <p class="text-center">-Atau-</p>
+                    
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                </form>
+                <div class="form-group ">
+                    <a href="{{ route('register') }}" class="text-decoration-none">
+                        <button type="submit" class="btn btn-danger btn-block">
+                            {{ __('Register') }}
+                        </button>
+                    </a>
                 </div>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <p class=" align-self-center">Atau</p>
+            </div>
+            <div class="col-md-4 text-uppercase">
+                <h3 class="font-weight-normal text-center mb-5">Masuk Dengan Sosial Media mu</h3>
+                <a href="#" class="btn btn-primary btn-block mt-5 py-2">Masuk Dengan Facebook</a>
+                <a href="#" class="btn btn-outline-primary btn-block mt-5 py-2">Masuk Dengan Google</a>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- tailwind -->
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
